47caca27583e0994ab5c7d21b9a2bf73966b4b79
Ben Thompson <ben.t11@hotmail.com>
2024-04-07 03:55:21 -0700

    formatting fixes

--- Program.cs
+++ Program.cs
@@ -59,8 +59,10 @@ public class BloggingContext : DbContext
     public DbSet<Blog> Blogs { get; set; }
     public DbSet<Post> Posts { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-        if (!optionsBuilder.IsConfigured) {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
             IConfigurationRoot conf = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
